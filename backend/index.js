d50fb437ccc6a96d243aa2386a3160b382358c49
Yusuf <yusufkhan1619@gmail.com>
2024-07-29 08:51:15 +0530

    "group fn"

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,66 @@ const app = express();
 const http = require("http").Server(app);
 const cors = require("cors");
 const PORT = 4000;
-
+const createUniqueID = () => {
+  return Math.random().toString(20).substring(2, 10);
+};
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://10.0.2.2:3000/",
+  },
+});
+let chatgroups = [];
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+socketIO.on("connection", (socket) => {
+  console.log(`${socket.id} user is just connected`);
+  socket.on("getAllGroups", () => {
+    socket.emit("groupList", chatgroups);
+  });
+  socket.on("createNewGroup", (currentGroupName) => {
+    console.log(currentGroupName);
+    chatgroups.unshift({
+      id: chatgroups.length + 1,
+      currentGroupName,
+      messages: [],
+    });
+    socket.emit("groupList", chatgroups);
+  });
+
+  socket.on("findGroup", (id) => {
+    const filteredGroup = chatgroups.filter((item) => item.id === id);
+    console.log("backend-----item", item);
+    console.log(
+      "filteredGroupfilteredGroupfilteredGroupfilteredGroup",
+      filteredGroup
+    );
+    socket.emit("foundGroup", filteredGroup[0].messages);
+  });
+
+  socket.on("newChatMessage", (data) => {
+    const { currentChatMessage, groupIdentifier, currentUser, timeDate } = data;
+    const filteredGroup = chatgroups.filter(
+      (item) => item.id === groupIdentifier
+    );
+    const newMessage = {
+      id: createUniqueID(),
+      text: currentChatMessage,
+      currentUser,
+      time: `${timeDate.hr}:${timeDate.mins}`,
+    };
+    socket
+      .to(filteredGroup[0].currentGroupName)
+      .emit("groupMessage", newMessage);
+    filteredGroup[0].messages.push(newMessage);
+    socket.emit("groupList", chatgroups);
+    socket.emit("foundGroup", filteredGroup[0].messages);
+  });
+});
+
 app.get("./api", (req, res) => {
-  console.log(req, res, "req,res========");
+  res.json(chatgroups);
 });
 
 http.listen(PORT, () => {
